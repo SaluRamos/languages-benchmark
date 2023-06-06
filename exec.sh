f6f162431a57699e8ebe63571c3de6e8965fa653
Risweb <ssh://webkriativa@ftp.webkriativa.com.br/~/repositorios/risweb_clinica.git>
2023-06-06 17:02:51 -0300

    generator rename, java 20 test, python 3.11.3 test, results.txt, utils.txt, cx_freeze test

--- exec.sh
+++ exec.sh
@@ -1,10 +1,9 @@
 clear
-rm -r build
-rm -r dist
-pyinstaller main.py
-cp data.txt dist/main/data.txt
-./dist/main/main
+python3 main.py
 javac Main.java
 java Main
 g++ main.cpp -o main
 ./main
+rm main
+rm Main.class
+rm main.spec
